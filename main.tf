9d3f1df0ed24a4871174f0d398f9ebf358c3600e
karan6467 <iamkaran37@gmail.com>
2026-05-30 15:34:43 +0530

    test

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.11.0"
+    }
+  }
+}
+ 
+provider "aws" {
+  region = "us-west-2"
+}
+ 
+resource "aws_instance" "myserver" {
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+ 
+  tags = {
+    Name = "Besant_instance" }
+}
+
+
+ 
